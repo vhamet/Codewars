4f1bcfae1c674b3c07d28f9337145e4d7fc9ef99
valentin <valentin.hamet@gmail.com>
2019-06-12 17:15:46 +0200

    Replace With Alphabet Position

--- 02 - Format a string of names like 'Bart, Lisa & Maggie'/index.js
+++ 02 - Format a string of names like 'Bart, Lisa & Maggie'/index.js
@@ -1,8 +1,6 @@
 const concatNames = (acc, current, index, arr) => index === 0 ? current 
   : (index === arr.length - 1 ? `${acc} & ${current}` : `${acc}, ${current}`);
 
-const list = (names) => {
-  return names.map(o => o.name).reduce(concatNames, '');
-}
+const list = (names) => names.map(o => o.name).reduce(concatNames, '');
 
 export { concatNames, list }
--- 03 - Replace With Alphabet Position/index.js
+++ 03 - Replace With Alphabet Position/index.js
@@ -1,7 +1,7 @@
 const alphabetPosition = (text) => text
   .toLowerCase()
   .split('')
-  .filter(c => /^[a-z]+$/.test(c))
+  .filter(c => /^[a-zA-Z]+$/.test(c))
   .map(c => c.charCodeAt(0) - 96)
   .join(' ');
 
